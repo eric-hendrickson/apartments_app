fd5e57173c6481740116adb00f46498ad6f2102a
Eric Hendrickson <hendrickson02@gmail.com>
2017-01-09 13:33:20 -0800

    Created the listing table, and rerouted it to route; feature testing complete, story 1 complete

--- spec/features/apartments_spec.rb
+++ spec/features/apartments_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.feature "Apartments", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+
+  context 'Going to the landing page' do
+    Steps 'Seeing the contennt of listings' do
+      Given 'I am on the landing page' do
+        visit '/'
+      end
+      Then 'I can see a header saying "Apartment Listings"' do
+        expect(page).to have_content("Apartment Listings")
+      end
+      And 'I can also see a table, with headers saying: Latitude, Longitude, Address 1, Address 2, City, State, Zip, Country, Name, Phone Number, Hours' do
+        expect(page).to have_content("Latitude")
+        expect(page).to have_content("Longitude")
+        expect(page).to have_content("Address 1")
+        expect(page).to have_content("Address 2")
+        expect(page).to have_content("City")
+        expect(page).to have_content("State")
+        expect(page).to have_content("Zip")
+        expect(page).to have_content("Country")
+        expect(page).to have_content("Name")
+        expect(page).to have_content("Phone Number")
+        expect(page).to have_content("Hours")
+      end
+    end
+  end
 end
